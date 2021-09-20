0ba95bb2498630a6186069e0b8aa0ed47120055a
hyozeen <hyozeen@kakao.com>
2021-09-20 09:16:35 +0900

    Add download btn

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+const elements = {};
+window.onload = function () {
+    this.getElements();
+    this.addClickEvent(elements.downloadBtn, this.downloadImage);
+};
+
+function getElements() {
+    elements.downloadBtn = this.document.getElementById('downloadBtn');
+    elements.canvas = this.document.getElementById('uploadedImg');
+}
+
+function addClickEvent(element, handler) {
+    element.addEventListener('click', handler);
+}
+
+function downloadImage() {
+    const dataUrl = elements.canvas.toDataURL('image/png');
+    elements.downloadBtn.href = dataUrl;
+}
+
 function getUploadedImage(files) {
     const file = files[0];
 
@@ -9,7 +29,7 @@ function getUploadedImage(files) {
     reader.onload = function (e) {
         const img = new Image();
         img.onload = function () {
-            const ctx = document.getElementById('uploadedImg').getContext('2d');
+            const ctx = elements.canvas.getContext('2d');
             ctx.drawImage(img, 0, 0, 800, 800);
         };
         img.src = e.target.result;
