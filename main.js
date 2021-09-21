cd68693c3e73450b151c77af700b6ab9443561ac
hyozeen <hyozeen@kakao.com>
2021-09-21 10:59:27 +0900

    Show pixel color when it's clicked

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const elements = {};
 window.onload = function () {
     this.getElements();
     this.addClickEvent(elements.downloadBtn, this.downloadImage);
+    this.addClickEvent(elements.canvas, this.showPixelColor);
 };
 
 function getElements() {
@@ -52,4 +53,41 @@ function drawGrid(number) {
     }
     context.strokeStyle = 'white';
     context.stroke();
+}
+
+function showPixelColor(e) {
+    const pos = findPos(this);
+    const x = e.pageX - pos.x;
+    const y = e.pageY - pos.y;
+    const coord = 'x=' + x + ', y=' + y;
+    const context = this.getContext('2d');
+    const pixel = context.getImageData(x, y, 1, 1).data;
+    const hex = '#' + ('000000' + rgbToHex(pixel[0], pixel[1], pixel[2])).slice(-6);
+    console.log(coord + ', ' + hex);
+}
+
+function findPos(obj) {
+    const curleft = 0;
+    const curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curleft += obj.offsetLeft;
+            curtop += obj.offsetTop;
+        } while ((obj = obj.offsetParent));
+        return { x: curleft, y: curtop };
+    }
+    return undefined;
+}
+
+function rgbToHex(r, g, b) {
+    if (r > 255 || g > 255 || b > 255) throw 'Invalid color component';
+    return ((r << 16) | (g << 8) | b).toString(16);
+}
+
+function randomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function randomColor() {
+    return `rgb(${randomInt(256)}, ${randomInt(256)}, ${randomInt(256)})`;
 }
